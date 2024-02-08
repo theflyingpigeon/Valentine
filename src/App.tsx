24f6082ae591356da6bb35bfaa412092422af00a
theflyingpigeon <ironwall89@gmail.com>
2024-02-09 00:11:04 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './tailwind.output.css';
+import Valentine from "./pages/Valentine";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1 className="text-3xl font-bold underline text-red-600">
+      <Valentine />
+    </h1>
+  );  
 }
 
 export default App;
