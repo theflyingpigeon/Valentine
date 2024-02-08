f9447f056ba2059cd246ba0b7beffef72a7dcce3
theflyingpigeon <ironwall89@gmail.com>
2024-02-09 00:18:30 +0100

    Added Tailwindcss tot the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './tailwind.output.css';
-import Valentine from "./pages/Valentine";
+// import Valentine from "./pages/Valentine";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      <Valentine />
-    </h1>
-  );  
+      <h1 className="text-3xl font-bold underline">
+          Hello world!
+      </h1>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './tailwind.css'
+import Valentine from "./pages/Valentine";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+    <React.StrictMode>
+        <Valentine />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
